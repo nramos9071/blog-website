f39701857cfa6f7e1dd17ac6716c489723008c84
Nick Ramos <nramos9071@gmail.com>
2024-07-24 01:05:45 -0500

    added light/dark mode

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,16 +2,53 @@ let userName = document.querySelector('#user-name')
 let blogTitle = document.querySelector('#blog-title')
 let blogPost = document.querySelector('#blog-post')
 let submitbtn = document.querySelector('#submit-button')
-const dark = document.querySelector('.dark')
+const darkBody = document.querySelector('.dark-body')
+const darkHeader = document.querySelector('.dark-header')
+const lightBody = document.querySelector('.light-body')
+const lightHeader = document.querySelector('.light-header')
+const lightMode = document.querySelector('#light-mode')
 let forms= document.getElementsByTagName('input')
 
-let mode = 'dark'
+let mode = 'dark-body'
 
 lightMode.addEventListener('click', function () {
 
-    if (mode === 'dark') {
+    event.preventDefault();
 
+    console.log('working');
+
+    if (mode === 'dark-body') {
+        mode = 'light-body'
+        darkBody.setAttribute('class', 'light-body')
+    } else {
+        
+        mode = 'dark-body'
+        darkBody.setAttribute('class', 'dark-body')
     }
+
+
+    
+})
+
+let mode1 = 'dark-header'
+
+lightMode.addEventListener('click', function () {
+
+    event.preventDefault();
+
+    console.log('working');
+
+    if (mode1 === 'dark-header') {
+        mode1 = 'light-header'
+        darkHeader.setAttribute('class', 'light-header')
+    } else {
+        
+        mode1 = 'dark-header'
+        darkHeader.setAttribute('class', 'dark-header')
+    }
+
+
+    
 })
 
 /*function sendAlert() {
