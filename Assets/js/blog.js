09ed31f3bc5238d0306cd5e1adf83b50f104420f
nramos9071 <nramos9071@gmail.com>
2024-07-25 12:22:13 -0500

    fixed object, object issue and styled post

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -4,8 +4,8 @@ const lightBody = document.querySelector('.light-body');
 const lightHeader = document.querySelector('.light-header');
 const lightMode = document.querySelector('#light-mode');
 let blogSection = document.getElementById('blog-section');
-/*const bringOver = JSON.parse(localStorage.getItem('blogSubmit'));*/
-let bringOver = localStorage.getItem('blogSubmit')
+const bringOver = JSON.parse(localStorage.getItem('blogSubmit'));
+
 
 
 let mode = 'dark-body'
@@ -49,23 +49,69 @@ lightMode.addEventListener('click', function () {
 
 
 
+
 function working() {
 
     const newPost = document.createElement('section');
     const content = document.createElement('p');
+
+    function addStyle() {
+        blogSection.classList.add('main-section')
+        newPost.classList.add('content-section1')
+    }
    
     
     blogSection.appendChild(newPost);
+    addStyle();
     newPost.appendChild(content);
-    content.textContent = bringOver;
-    console.log(bringOver)
+    content.textContent = bringOver.userName;
+    
 
-  
 
-   
-  
-   
+
+    console.log(bringOver.userName);
+
     
 }
 
-blogSection.addEventListener('load', working())
+function working2() {
+
+    const newPost2 = document.createElement('section');    
+    const content2 = document.createElement('p');
+
+    function addStyle2() {
+        newPost2.classList.add('content-section2')
+    }
+   
+    blogSection.appendChild(newPost2);
+    addStyle2();
+    newPost2.appendChild(content2);
+    content2.textContent = bringOver.blogTitle;
+    
+    console.log(bringOver.blogTitle);
+
+       
+}
+
+function working3() {
+
+    const newPost3 = document.createElement('section');
+    const content3 = document.createElement('p');
+
+    function addStyle3() {
+        newPost3.classList.add('content-section3')
+    }
+   
+    blogSection.appendChild(newPost3);
+    addStyle3();
+    newPost3.appendChild(content3);
+    content3.textContent = bringOver.blogPost;
+    
+    console.log(bringOver.blogPost);
+
+
+
+    
+}
+
+blogSection.addEventListener('load', working(), working2(), working3())
